d9362a1d0d3b4b4f27e5ddf4fa039fde400aa3e6
fredleluly <fredleluly@gmail.com>
2021-02-14 13:03:46 +0700

    Change Simple decrypt algo with short version

--- Simple decrypt algo.py
+++ Simple decrypt algo.py
@@ -18,9 +18,15 @@ def decrypt(key):
         else:
             o += str(0)
     return(o)
+# if you want short one
+import string
+def decrypt(test_key):
+    return( ''.join([str(test_key.count(i))for i in string.ascii_lowercase]))
+
 print(decrypt("$aaaa#bbb*ccfff!z"))
 print(decrypt("z$aaa#ccc%eee1234567890"))
 #print(decrypt("$aaaa#bbb*ccfff!z"))
+
 """
 z$aaa#ccc%eee1234567890
 """
